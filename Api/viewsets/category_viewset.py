21a2d816e64eb7e8b7cc68cd7a900b10a13ffa3b
antonarnaudov <antonarnaudov76@gmail.com>
2021-04-18 00:06:07 +0300

    Pagination and Search added to Category and Photos APIs

--- Api/viewsets/category_viewset.py
+++ Api/viewsets/category_viewset.py
@@ -1,4 +1,7 @@
 from rest_framework.generics import ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -13,6 +16,18 @@ from Photos.models.category_model import Category
 #         serializer = CategorySerializer(category, many=True)
 #         return Response(serializer.data)
 
+
+# get current page number from offset + 1
+
+class CategoryPagination(LimitOffsetPagination):
+    default_limit = 1
+    max_limit = 100
+
+
 class CategoryListApiView(ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    # filter_fields = ('id', )
+    search_fields = ('category',)
+    pagination_class = CategoryPagination
--- Api/viewsets/photos_viewset.py
+++ Api/viewsets/photos_viewset.py
@@ -1,4 +1,7 @@
 from rest_framework.generics import ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -6,6 +9,15 @@ from Api.serializers.photos_serializer import PhotosSerializer
 from Photos.models.photos_model import Photos
 
 
+class PhotosPagination(LimitOffsetPagination):
+    default_limit = 1
+    max_limit = 100
+
+
 class PhotosListApiView(ListAPIView):
     queryset = Photos.objects.all()
     serializer_class = PhotosSerializer
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    # filter_fields = ('price', )
+    search_fields = ('name', )
+    pagination_class = PhotosPagination
