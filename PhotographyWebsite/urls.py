6e97194bf8e7a2db5175e81c113747a3ec2b3e0b
Anton Arnaudov <anton.arnaudov@audienceplatform.tv>
2023-07-17 20:31:55 +0300

    Production settings

--- PhotographyWebsite/urls.py
+++ PhotographyWebsite/urls.py
@@ -37,19 +37,21 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
-media_files = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 urlpatterns = [
-      path('admin/', admin.site.urls),
-      path('', include('Photos.urls')),
-      path('auth/', include('Auth.urls')),
-      path('api/', include('Api.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('Photos.urls')),
+    path('auth/', include('Auth.urls')),
+    path('api/', include('Api.urls')),
 
-      # Swagger
-      path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-      path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # Swagger
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-      url(r'^swagger(?P<format>\.json|\.yaml)$',
-          schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0), name='schema-json'),
 
-  ] + media_files
+]
+
+# add at the last
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
